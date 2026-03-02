397fbc81363ac398ccafdd121460aa34a0f52a98
Ooble5 <dhagmannsmith@gmail.com>
2026-03-02 03:32:26 -0500

    Add dump of freeCodeCamp Universe Challenge

--- universe.sql
+++ universe.sql
@@ -16,6 +16,494 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: galaxy; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.galaxy (
+    galaxy_id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    type_of_galaxy character varying(30),
+    diameter_in_light_years numeric NOT NULL,
+    year_of_discovery integer
+);
+
+
+ALTER TABLE public.galaxy OWNER TO freecodecamp;
+
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.galaxy_galaxy_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.galaxy_galaxy_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.galaxy_galaxy_id_seq OWNED BY public.galaxy.galaxy_id;
+
+
+--
+-- Name: moon; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.moon (
+    moon_id integer NOT NULL,
+    planet_id integer,
+    name character varying(30) NOT NULL,
+    named_after_a_god boolean,
+    size_compared_to_earth_moon numeric NOT NULL
+);
+
+
+ALTER TABLE public.moon OWNER TO freecodecamp;
+
+--
+-- Name: moon_moon_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.moon_moon_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.moon_moon_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: moon_moon_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.moon_moon_id_seq OWNED BY public.moon.moon_id;
+
+
+--
+-- Name: planet; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.planet (
+    planet_id integer NOT NULL,
+    star_id integer,
+    name character varying(30) NOT NULL,
+    type_of_planet character varying(30) NOT NULL,
+    diameter_in_km numeric,
+    distance_from_own_star_in_km numeric
+);
+
+
+ALTER TABLE public.planet OWNER TO freecodecamp;
+
+--
+-- Name: planet_planet_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.planet_planet_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.planet_planet_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: planet_planet_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.planet_planet_id_seq OWNED BY public.planet.planet_id;
+
+
+--
+-- Name: star; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.star (
+    star_id integer NOT NULL,
+    galaxy_id integer,
+    name character varying(30) NOT NULL,
+    type_of_star character varying(30),
+    diameter_in_km numeric NOT NULL,
+    star_is_alive boolean,
+    planets_orbiting integer
+);
+
+
+ALTER TABLE public.star OWNER TO freecodecamp;
+
+--
+-- Name: star_star_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.star_star_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.star_star_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: star_star_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.star_star_id_seq OWNED BY public.star.star_id;
+
+
+--
+-- Name: tidbit; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.tidbit (
+    tidbit_id integer NOT NULL,
+    galaxy_id integer,
+    star_id integer,
+    planet_id integer,
+    moon_id integer,
+    fact text NOT NULL,
+    source text NOT NULL,
+    name character varying(40)
+);
+
+
+ALTER TABLE public.tidbit OWNER TO freecodecamp;
+
+--
+-- Name: tidbit_tidbit_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.tidbit_tidbit_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tidbit_tidbit_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: tidbit_tidbit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.tidbit_tidbit_id_seq OWNED BY public.tidbit.tidbit_id;
+
+
+--
+-- Name: galaxy galaxy_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.galaxy ALTER COLUMN galaxy_id SET DEFAULT nextval('public.galaxy_galaxy_id_seq'::regclass);
+
+
+--
+-- Name: moon moon_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon ALTER COLUMN moon_id SET DEFAULT nextval('public.moon_moon_id_seq'::regclass);
+
+
+--
+-- Name: planet planet_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.planet ALTER COLUMN planet_id SET DEFAULT nextval('public.planet_planet_id_seq'::regclass);
+
+
+--
+-- Name: star star_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.star_star_id_seq'::regclass);
+
+
+--
+-- Name: tidbit tidbit_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit ALTER COLUMN tidbit_id SET DEFAULT nextval('public.tidbit_tidbit_id_seq'::regclass);
+
+
+--
+-- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.galaxy (galaxy_id, name, type_of_galaxy, diameter_in_light_years, year_of_discovery) FROM stdin;
+1	Milky Way	Spiral Galaxy	100000	\N
+3	Large Magellanic Cloud	Magellanic Spiral	32200	\N
+4	Small Magellanic Cloud	Dwarf Irregular Galaxy	18900	\N
+2	Andromeda Galaxy	Spiral Galaxy	152000	1923
+5	Ursa Major III	Dwarf Spheroidal Galaxy	19.6	2023
+6	Sag DEG	Dwarf Elliptical Galaxy	10000	1994
+\.
+
+
+--
+-- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.moon (moon_id, planet_id, name, named_after_a_god, size_compared_to_earth_moon) FROM stdin;
+1	3	The Moon	f	1
+2	4	Phobos	t	0.0064
+3	4	Deimos	t	0.0045
+4	5	Ganymede	f	1.52
+5	5	Europa	f	0.9
+6	5	Io	f	1.05
+7	5	Callisto	f	1.39
+8	5	Himalia	f	0.05
+9	5	Amalthea	f	0.05
+10	6	Enceladus	f	0.15
+11	6	Titan	f	1.48
+12	6	Rhea	t	0.44
+13	6	Iapetus	t	0.42
+14	6	Dione	t	0.32
+15	7	Puck	f	0.05
+16	7	Titania	f	0.45
+17	7	Oberon	f	0.44
+18	8	Triton	t	0.78
+19	8	Proteus	t	0.12
+20	8	Nereid	f	0.1
+\.
+
+
+--
+-- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.planet (planet_id, star_id, name, type_of_planet, diameter_in_km, distance_from_own_star_in_km) FROM stdin;
+1	1	Mercury	Terrestrial	4879.4	57910006
+2	1	Venus	Terrestrial	12104	108199995
+3	1	Earth	Terrestrial	12742	149599951
+4	1	Mars	Terrestrial	6779	227939920
+5	1	Jupiter	Gas Giant	139820	778330257
+6	1	Saturn	Gas Giant	116460	1429400028
+7	1	Uranus	Neptunian	50724	2870989228
+8	1	Neptune	Neptunian	49244	4504299579
+9	5	TRAPPIST-1d	Terrestrial	9989.7	3333000
+10	5	TRAPPIST-1e	Terrestrial	11595	4400000
+11	6	Proxima Centauri b	Terrestrial	\N	7253000
+12	6	Proxima Centauri d	Terrestrial	\N	\N
+\.
+
+
+--
+-- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.star (star_id, galaxy_id, name, type_of_star, diameter_in_km, star_is_alive, planets_orbiting) FROM stdin;
+2	1	Sirius A	A-Type Star	2400000	t	\N
+3	1	Sirius B	White Dwarf	12000	f	\N
+4	3	WOH G64	Red Supergiant	1113120000	t	\N
+1	1	The Sun	Yellow Dwarf Star	1392700	t	8
+5	1	TRAPPIST-1	Red Dwarf Star	168360	t	7
+6	1	Proxima Centauri	Red Dwarf Star	201840	t	3
+\.
+
+
+--
+-- Data for Name: tidbit; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.tidbit (tidbit_id, galaxy_id, star_id, planet_id, moon_id, fact, source, name) FROM stdin;
+1	2	\N	\N	\N	This galaxy is on a collision course with our galaxy	wikipedia	Fact about the Andromeda Galaxy
+2	\N	2	\N	\N	This is the brightest star in Earth's night sky	britannica	Fact about Sirius A
+3	\N	\N	\N	4	This is the largest moon in our solar system. It is even bigger than Mercury	nasa.gov	Fact about Ganymede
+\.
+
+
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 6, true);
+
+
+--
+-- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 20, true);
+
+
+--
+-- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 12, true);
+
+
+--
+-- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.star_star_id_seq', 6, true);
+
+
+--
+-- Name: tidbit_tidbit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.tidbit_tidbit_id_seq', 3, true);
+
+
+--
+-- Name: galaxy galaxy_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_name_key UNIQUE (name);
+
+
+--
+-- Name: galaxy galaxy_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.galaxy
+    ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
+
+
+--
+-- Name: moon moon_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_name_key UNIQUE (name);
+
+
+--
+-- Name: moon moon_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_pkey PRIMARY KEY (moon_id);
+
+
+--
+-- Name: planet planet_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_name_key UNIQUE (name);
+
+
+--
+-- Name: planet planet_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_pkey PRIMARY KEY (planet_id);
+
+
+--
+-- Name: star star_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_name_key UNIQUE (name);
+
+
+--
+-- Name: star star_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_pkey PRIMARY KEY (star_id);
+
+
+--
+-- Name: tidbit tidbit_fact_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_fact_key UNIQUE (fact);
+
+
+--
+-- Name: tidbit tidbit_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_pkey PRIMARY KEY (tidbit_id);
+
+
+--
+-- Name: moon moon_planet_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_planet_id_fkey FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+
+
+--
+-- Name: planet planet_star_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+--
+-- Name: star star_galaxy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
+
+--
+-- Name: tidbit tidbit_galaxy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
+
+--
+-- Name: tidbit tidbit_moon_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_moon_id_fkey FOREIGN KEY (moon_id) REFERENCES public.moon(moon_id);
+
+
+--
+-- Name: tidbit tidbit_planet_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_planet_id_fkey FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+
+
+--
+-- Name: tidbit tidbit_star_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.tidbit
+    ADD CONSTRAINT tidbit_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
 --
 -- PostgreSQL database dump complete
 --
